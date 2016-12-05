9600589b26e08222311522c34794a7ed161bc0d9
轻色年华 <13675626825@163.com>
2016-12-05 19:54:03 +0800

    更换跳转界面

--- app/config.php
+++ app/config.php
@@ -141,8 +141,8 @@ return [
     // 视图输出字符串内容替换
     'view_replace_str'       => [],
     // 默认跳转页面对应的模板文件
-    'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
-    'dispatch_error_tmpl'    => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
+    'dispatch_success_tmpl'  => ROOT_PATH . DS . 'template' . DS . 'common' . DS . 'dispatch_jump.html',
+    'dispatch_error_tmpl'    => ROOT_PATH . DS . 'template' . DS . 'common' . DS . 'dispatch_jump.html',
 
     // +----------------------------------------------------------------------
     // | 异常及错误设置
