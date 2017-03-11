c735070fbbfd9aad14af76bb333be8fb0b1fb953
轻色年华 <13675626825@163.com>
2017-03-11 22:29:32 +0800

    优化行为钩子参数传递

--- app/behavior/SystemBehaviorInit.php
+++ app/behavior/SystemBehaviorInit.php
@@ -8,49 +8,49 @@ class SystemBehaviorInit
     /** 应用初始化 */
     public function app_init(&$param)
     {
-        Tree::hook()->listen('app-init');
+        Tree::hook()->listen('app-init', $param);
     }
 
     /** 应用开始标签位 */
     public function app_begin(&$param)
     {
-        Tree::hook()->listen('app-begin');
+        Tree::hook()->listen('app-begin', $param);
     }
 
     /** 模块初始化标签位 */
     public function module_init(&$param)
     {
-        Tree::hook()->listen('module-init');
+        Tree::hook()->listen('module-init', $param);
     }
 
     /** 控制器开始标签位 */
     public function action_begin(&$param)
     {
-        Tree::hook()->listen('action-begin');
+        Tree::hook()->listen('action-begin', $param);
     }
 
     /** 视图输入过滤标签位 */
     public function view_filter(&$param)
     {
-        Tree::hook()->listen('view-filter');
+        Tree::hook()->listen('view-filter', $param);
     }
 
     /** 应用结束标签位 */
     public function app_end(&$param)
     {
-        Tree::hook()->listen('app-end');
+        Tree::hook()->listen('app-end', $param);
     }
 
     /** 日志Write标签位 */
     public function log_write(&$param)
     {
-        Tree::hook()->listen('log-write');
+        Tree::hook()->listen('log-write', $param);
     }
 
     /** 输出结束标签位 */
     public function response_end(&$param)
     {
-        Tree::hook()->listen('response-end');
+        Tree::hook()->listen('response-end', $param);
     }
 
 }
